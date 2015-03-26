4ee7267d1491604210dd5f8acc0425b68ad36e0e
Pavel Strnad <pavel.strnad@fel.cvut.cz>
2015-03-26 16:58:17 +0100

    fixed bugs in NCF

--- mixgene_project/wrappers/walkforest/__init__.py
+++ mixgene_project/wrappers/walkforest/__init__.py
@@ -1,8 +1,1 @@
-# -*- coding: utf-8 -*-
-"""
-Created on Fri Oct 24 15:35:15 2014
-
-@author: andelmi2
-"""
-
-from walkforest import *
+# -*- coding: utf-8 -*-"""Created on Fri Oct 24 15:35:15 2014@author: andelmi2"""from walkforest import *
